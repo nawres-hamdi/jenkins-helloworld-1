7071e7bc4eb4e99d94977eb0e62e3b7be5de0a13
nawres-hamdi <126500642+nawres-hamdi@users.noreply.github.com>
2023-04-17 17:58:47 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
 pipeline {
- agent any
- stages {
-   stage('clone') {
-      steps {
-        sh "rm -rf *"
-        sh "git clone https://github.com/nawres-hamdi/jenkins-helloworld-1"
-      }
-   }
-   stage('build') {
-      steps {
-        sh "cd jenkins-helloworld-1/ && javac Main.java"
-      }
-   }
-   stage('run' ) {
-      steps {
-        sh "cd jenkins-helloworld-1/ && java Main"
-      }
-   }
- }
+  agent any
+  stages {
+    stage('clone') {
+       steps {
+         sh "rm -rf *"
+         sh "git clone https://github.com/nawres-hamdi/jenkins-helloworld-1"
+       }
+    }
+    stage('build') {
+        steps {
+         sh "cd jenkins-helloworld-1/ && javac Main.java"
+       }
+    }
+    stage('run' ) {
+        steps {
+         sh "cd jenkins-helloworld-1/ && java Main"
+        }
+    }
+  }
 }
